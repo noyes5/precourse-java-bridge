b0a6d3e89b6b3cedf1b32396eaa4e06ea5422702
noyes5 <noyes5@naver.com>
2023-12-01 16:28:53 +0900

    refactor(GameAction): 메소드 이름 리팩토링

--- src/test/java/bridge/domain/GameActionTest.java
+++ src/test/java/bridge/domain/GameActionTest.java
@@ -3,14 +3,16 @@ package bridge.domain;
 import static bridge.util.BridgeException.ERROR_GAME_COMMAND;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class GameActionTest {
 
+    @DisplayName("유효하지 않은 값이 들어오면 예외 테스트")
     @ParameterizedTest
     @ValueSource(strings = {"A", "B", "재시작", "12312"})
-    void 잘못된_입력값(String input) {
+    void 유효하지않은_입력_검증(String input) {
         assertThatThrownBy(() -> GameAction.from(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ERROR_GAME_COMMAND.getMessage());
